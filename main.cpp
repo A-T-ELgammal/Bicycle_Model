a71d3b9b1742d536fb71c70f8ed5388ad653f2d7
Ahmed Elgammal <a.t.elgammal@gmail.com>
2023-12-14 20:22:36 +0200

    implementing_class_object)_using_test_cases

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <cassert>
-#include "Data"
+#include <vector>
+
 #include "Bicycle_Model"
 
 using namespace std;
@@ -8,25 +8,24 @@ using namespace std;
 int main()
 {
     BicycleModel bicycle(2.5, M_PI / 4, 0.1);
+    vector<ModelParams> testCases = {
+        make_tuple(0.0F, 0.0F, float(M_PI / 4), 5.0F, 2.0F, float(M_PI / 8)),
+        make_tuple(1.0F, 2.0F, 0.0F, 3.0F, 1.0F, float(M_PI / 4)),
+        make_tuple(-1.0F, -2.0F, float(M_PI / 2), 4.0F, -1.0F, float(-M_PI / 4))};
 
-    for (int i = 0; i < testCases.size(); i++)
+    for (const ModelParams &testCase : testCases)
     {
-        assert(1 == 2);
+        const auto [x, y, theta, velocity, acceleration, steeringAngle] = testCase;
+        const ModelParams updated = bicycle.update(x, y, theta, velocity, acceleration, steeringAngle);
 
-        // assert(unewVelocity == expectedX);
-        // assert(uangleVelocity == expectedY);
-        // assert(unewX == expectedTheta);
-        // assert(unewY == expectedVelocity);
-        // assert(unewTheta == expectedsteeringAngle);
-        // assert(unewTheta == expectedangleVelocity);
-
-        // cout << "Test Case: "
-        //      << "ux: " << unewX << ", "
-        //      << "uy: " << unewY << ", "
-        //      << "utheta: " << unewTheta << ", "
-        //      << "uvelocity: " << unewVelocity << ", "
-        //      << "usteeringAngle: " << usteeringAngle << ", "
-        //      << "uacceleration: " << uangleVelocity << endl;
+        cout << "Updated x: " << get<0>(updated) << endl;
+        cout << "Updated y: " << get<1>(updated) << endl;
+        cout << "Updated theta: " << get<2>(updated) << endl;
+        cout << "Updated velocity: " << get<3>(updated) << endl;
+        cout << "Updated acceleration: " << get<4>(updated) << endl;
+        cout << "Updated steeringAngle: " << get<5>(updated) << endl;
+        cout << "==========================================" << endl;
     }
+
     return 0;
 }
